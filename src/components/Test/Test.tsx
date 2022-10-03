5221bd01de5d03cf66cfdad17698dfb6bfb2ae58
Vikram Chauhan <vikram.coder1@gmail.com>
2022-10-03 12:42:52 +0530

    Types added

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
+import { TestProps } from "./Test.types";
 import "../../../node_modules/@allui/css/dist/allui.css";
 
-const Test = () => {
-  return <div>Test 5</div>;
+const Test: FC<TestProps> = ({ text }) => {
+  return <h1>{text}</h1>;
 };
 
 export default Test;
